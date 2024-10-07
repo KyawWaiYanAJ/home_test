ae272b8688bff688d0fdaa824fbc05170a5f1ac4
vagrant <vagrant@hellocloud-native-box>
2024-10-07 14:04:23 +0000

    Enable JWT Auth

---
+++ jwt-auth-method/main.tf
@@ -0,0 +1,72 @@
+resource "vault_jwt_auth_backend" "tfc_jwt" {
+    description         = "Demonstration of the Terraform JWT auth backend"
+    path                = "jwt"
+    oidc_discovery_url  = "https://app.terraform.io"
+    bound_issuer        = "https://app.terraform.io"
+}
+
+resource "vault_policy" "admin_policy" {
+  name = "admin-policy"
+
+  policy = <<EOT
+path "auth/*" {
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+
+# Create, update, and delete auth methods
+path "sys/auth/*" {
+  capabilities = ["create", "update", "delete", "sudo"]
+}
+
+# List auth methods
+path "sys/auth" {
+  capabilities = ["read"]
+}
+
+# Enable and manage the key/value secrets engine at `secret/` path
+# List, create, update, and delete key/value secrets
+path "secret/*" {
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+
+# Manage secrets engines
+path "sys/mounts/*" {
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+
+# List existing secrets engines.
+path "sys/mounts" {
+  capabilities = ["read"]
+}
+
+path "aws-master-account/" {
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+
+path "aws-master-account/*" {
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+
+path "sys/policy/*" {
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+
+path "sys/policy/" {
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+EOT
+}
+
+resource "vault_jwt_auth_backend_role" "tfc_admin_role" {
+  backend = vault_jwt_auth_backend.tfc_jwt.path
+  role_name = "admin-role"
+  token_policies = [vault_policy.admin_policy.name]
+  bound_audiences = ["vault.workload.identity"]
+  bound_claims_type = "glob"
+  bound_claims = {
+    sub = "organization:hellocloud-corhort7:project:Default Project:workspace:hellocloud-cohort7:run_phase:*"
+  }
+  user_claim = "terraform_full_workspace"
+  role_type  = "jwt"
+  token_ttl  = 1200
+}
