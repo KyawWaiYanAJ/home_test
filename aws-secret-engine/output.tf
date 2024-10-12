658a5717e9e5176bf3158ceea5b9f4a564513a21
vagrant <vagrant@hellocloud-native-box>
2024-10-12 05:19:50 +0000

    Test3

--- aws-secret-engine/output.tf
+++
@@ -1,8 +0,0 @@
-output "acces_key" {
-  value = data.vault_aws_access_credentials.creds.access_key
-  sensitive = true
-}
-output "secret_key" {
-  value = data.vault_aws_access_credentials.creds.secret_key
-  sensitive = true
-}
