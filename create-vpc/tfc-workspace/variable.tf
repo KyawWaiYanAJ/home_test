1280d0815eafe5af992c776174b3e97fbfd88074
vagrant <vagrant@hellocloud-native-box>
2024-10-18 18:26:55 +0000

    Upate Chanages

--- create-vpc/tfc-workspace/variable.tf
+++ create-vpc/tfc-workspace/variable.tf
@@ -1,12 +1,12 @@
 variable "workspace_name" {
   description = "Workspace Name"
   type = string
-  default = "vpc-peering-day5"
+  default = "create_vpc"
 }
 variable "org_name" {
   description = "Organization Name"
   type = string
-  default = "SecureOps"
+  default = "Secure_Ops"
 }
 variable "vault_url" {
   description = "The address of the Vault instance runs will access."
@@ -22,29 +22,4 @@ variable "vault_namespace" {
   description = "TFC_VAULT_NAMESPACE"
   type = string
   default = "admin"
-}
-variable "aws_region" {
-  description = "AWS_REGION"
-  type = string
-  default = "ap-southeast-1"
-}
-variable "tfc_vault_backed_aws_auth" {
-  description = "TFC_VAULT_BACKED_AWS_AUTH"
-  type = string
-  default = "true"
-}
-variable "tfc_vault_backed_aws_auth_type" {
-  description = "TFC_VAULT_BACKED_AWS_AUTH_TYPE "
-  type = string
-  default = "iam_user"
-}
-variable "tfc_vault_backed_aws_run_vault_role" {
-  description = "TFC_VAULT_BACKED_AWS_RUN_VAULT_ROLE"
-  type = string
-  default = "admin-access-role"
-}
-variable "tfc_vault_backed_aws_mount_path" {
-  description = "TFC_VAULT_BACKED_AWS_MOUNT_PATH"
-  type = string
-  default = "aws-master-account"
 }
