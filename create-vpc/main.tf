e4bd10112c52e70e8f0430c74ed93a23cff4d413
KyawWaiYanAJ <112500145+KyawWaiYanAJ@users.noreply.github.com>
2024-10-19 01:56:42 +0800

    Update main.tf

--- create-vpc/main.tf
+++ create-vpc/main.tf
@@ -1,8 +1,6 @@
 # AWS Provider Configuration
-# provider "aws" {
-#   access_key = data.vault_aws_access_credentials.creds.access_key
-#   secret_key = data.vault_aws_access_credentials.creds.secret_key
-# }
+provider "aws" {
+}
 
 # Create a VPC for the application
 resource "aws_vpc" "app_vpc" {
