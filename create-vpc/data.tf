693f6b4b97dd703dbfda6ebc0102c2f62eecd43d
vagrant <vagrant@hellocloud-native-box>
2024-10-18 19:09:25 +0000

    update

--- create-vpc/data.tf
+++ create-vpc/data.tf
@@ -2,7 +2,7 @@ data "aws_availability_zones" "azs" {
   state = "available"
 }
 
-data "vault_aws_access_credentials" "creds" {
-   backend = "aws-master-account"
-   role    = "admin-access-role"
-}
+#data "vault_aws_access_credentials" "creds" {
+#   backend = "aws-master-account"
+#   role    = "admin-access-role"
+#}
